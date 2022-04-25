2760a3ae73fd73b47c817d6afe8cdcd80fc17083
Lana Vlasenko <lanachka1993@gmail.com>
2022-04-25 20:17:24 +0300

    Initial commit.

---
+++ src/Mathematics.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Mathematics;
+
+class Mathematics
+{
+    public function sum($a, $b)
+    {
+        return $a + $b;
+    }
+
+    public function multiply($a, $b)
+    {
+        return $a * $b;
+    }
+}
